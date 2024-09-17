d986bf53272e3549d6388609e37780150bf3dc73
Dangiji121 <chandandihuri88@gmail.com>
2024-09-17 21:53:15 +0530

    pdf.js
    code for print blog post

---
+++ pdf.js
@@ -0,0 +1,15 @@
+function printBlogPost() {
+            var printContent = document.getElementById('blogContent').innerHTML;
+            var newWindow = window.open('', '', 'height=600,width=800');
+            newWindow.document.write(
+                '<html><head><title>Print Blog Post</title>' +
+                '<style>.blog-content { font-family: Arial, sans-serif; margin: 20px; }</style>' +
+                '</head><body>' +
+                printContent +
+                '</body></html>'
+            );
+            newWindow.document.close();
+            newWindow.focus();
+            newWindow.print();
+            newWindow.close();
+        }
